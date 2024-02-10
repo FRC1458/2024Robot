2f905bb71a91269a74ff6a8eadcd3ff8fd600bb6
Victor2275 <gusev0219@gmail.com>
2024-02-10 10:17:33 -0800

    another error in swerveDrive, quick fix

--- src/main/java/frc/robot/swervedrive/SwerveDrive.java
+++ src/main/java/frc/robot/swervedrive/SwerveDrive.java
@@ -164,7 +164,7 @@ public class SwerveDrive {
     public void resetNavX(Pose2d currentRobotPosition) {
         navX.reset();
         odometry.resetPosition(Rotation2d.fromDegrees(navX.getAngle()), getModulePositions(), currentRobotPosition);
-        SmartDashboard.putNumber("Swervedrive position: " + odometry.getEstimatedPosition());
+        //SmartDashboard.putNumber("Swervedrive position: ",  odometry.getEstimatedPosition());
     }
 
     public void resetMaxVel() {
