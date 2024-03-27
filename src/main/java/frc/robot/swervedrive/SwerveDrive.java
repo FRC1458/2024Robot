462acdae8bf655eb32a67bb69df41c9152582641
Tearless Cape <jeremyphan50@gmail.com>
2024-03-26 17:39:19 -0700

    Allow different rotation at odometry reset

--- src/main/java/frc/robot/swervedrive/SwerveDrive.java
+++ src/main/java/frc/robot/swervedrive/SwerveDrive.java
@@ -199,7 +199,7 @@ public class SwerveDrive {
         frontRight.resetOdometry();
         backLeft.resetOdometry();
         backRight.resetOdometry();
-        odometry.resetPosition(new Rotation2d().minus(navX.getRotation2d()), getPositions(), pose);
+        odometry.resetPosition(pose.getRotation().minus(navX.getRotation2d()), getPositions(), pose);
     }
 
     public Pose2d getPose() {
