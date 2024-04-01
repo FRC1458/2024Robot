74954a7b1312d28443f347ac63ef482e1023cc34
Vivaan Patnaik <vivpat52@gmail.com>
2024-04-01 16:28:39 -0700

    Tuned
    motor has no load so we need to retune for shooter. But for now this will go to robot shotoer code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,8 +15,8 @@ import frc.robot.wrappers.XboxControllerWrapper;
 import static com.ctre.phoenix6.signals.NeutralModeValue.Coast;
 import static frc.robot.RobotConstants.*;
 
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.configs.*;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 
 
 
@@ -24,17 +24,20 @@ public class Robot extends TimedRobot {
   private TalonFX motor;
   private TalonFXConfiguration motorConfig;
 
-  double ks = 0.25;
-  double kv = 0.12;
-  double ka = 0.01;
-  double kp = 4.8;
+  Slot0Configs slot0configs = new Slot0Configs();
+
+  double ks = 0.01;
+  double kv = 0.11;
+  //double ka = 0.01;
+  double kp = .10;
   double ki = 0;
-  double kd = 0.1;
+  double kd = 0;
+
 
   public Robot() {
     super(0.03);
 
-    motor = new TalonFX(51);  
+    motor = new TalonFX(50);  
     motorConfig = new TalonFXConfiguration();
 
   }
@@ -43,40 +46,44 @@ public class Robot extends TimedRobot {
   }
   @Override
   public void teleopInit() {
-    motorConfig.Slot0.kS = ks; // Add 0.25 V output to overcome static friction
-    motorConfig.Slot0.kV = kv; // A velocity target of 1 rps results in 0.12 V output
-    motorConfig.Slot0.kA = ka; // An acceleration of 1 rps/s requires 0.01 V output
-    motorConfig.Slot0.kP = kp; // A position error of 2.5 rotations results in 12 V output
-    motorConfig.Slot0.kI = ki; // no output for integrated error
-    motorConfig.Slot0.kD = kd; // A velocity error of 1 rps results in 0.1 V output
-    motorConfig.MotionMagic.MotionMagicCruiseVelocity = 0; //80;
-    motorConfig.MotionMagic.MotionMagicAcceleration = 160;
-    motorConfig.MotionMagic.MotionMagicJerk = 1600;
-    motor.getConfigurator().apply(motorConfig);
+    slot0configs.kS = ks; // Add 0.25 V output to overcome static friction
+    slot0configs.kV = kv; // A velocity target of 1 rps results in 0.12 V output
+    //slot0configs.kA = ka; // An acceleration of 1 rps/s requires 0.01 V output
+    slot0configs.kP = kp; // A position error of 2.5 rotations results in 12 V output
+    slot0configs.kI = ki; // no output for integrated error
+    slot0configs.kD = kd; // A velocity error of 1 rps results in 0.1 V output
+    //motorConfig.MotionMagic.MotionMagicCruiseVelocity = 0; //80;
+    //motorConfig.MotionMagic.MotionMagicAcceleration = 160;
+    //motorConfig.MotionMagic.MotionMagicJerk = 1600;
+    motor.getConfigurator().apply(slot0configs);
     SmartDashboard.putNumber("motor speed (percent)", 0.25);
     SmartDashboard.putNumber("kS", ks);
     SmartDashboard.putNumber("kV", kv);
-    SmartDashboard.putNumber("kA", ka);
+    //SmartDashboard.putNumber("kA", ka);
     SmartDashboard.putNumber("kP", kp);
     SmartDashboard.putNumber("kI", ki);
     SmartDashboard.putNumber("kD", kd);
     SmartDashboard.putNumber("MMCruiseVelocity", 0);
     SmartDashboard.putNumber("MMAcceleration", 160);
     SmartDashboard.putNumber("MMJerk", 1600);
+    SmartDashboard.putNumber("motor speed (rps)", 0);
   }
   @Override
   public void teleopPeriodic() {
     SmartDashboard.putNumber("Rotor Velocity", (double) motor.getRotorVelocity().getValue());
-    motorConfig.Slot0.kS = SmartDashboard.getNumber("kS", ks); // Add 0.25 V output to overcome static friction
-    motorConfig.Slot0.kV = SmartDashboard.getNumber("kV", kv); // A velocity target of 1 rps results in 0.12 V output
-    motorConfig.Slot0.kA = SmartDashboard.getNumber("kA", ka); // An acceleration of 1 rps/s requires 0.01 V output
-    motorConfig.Slot0.kP = SmartDashboard.getNumber("kP", kp); // A position error of 2.5 rotations results in 12 V output
-    motorConfig.Slot0.kI = SmartDashboard.getNumber("kI", ki); // no output for integrated error
-    motorConfig.Slot0.kD = SmartDashboard.getNumber("kD", kd);
-    motorConfig.MotionMagic.MotionMagicCruiseVelocity = SmartDashboard.getNumber("MMCruiseVelocity", 0);
-    motorConfig.MotionMagic.MotionMagicAcceleration = SmartDashboard.getNumber("MMAcceleration", 160);
-    motorConfig.MotionMagic.MotionMagicJerk = SmartDashboard.getNumber("MMJerk", 1600);
-    motor.set(SmartDashboard.getNumber("motor speed (percent)", 0.25));
+    slot0configs.kS = SmartDashboard.getNumber("kS", ks); // Add 0.25 V output to overcome static friction
+    slot0configs.kV = SmartDashboard.getNumber("kV", kv); // A velocity target of 1 rps results in 0.12 V output
+    //slot0configs.kA = SmartDashboard.getNumber("kA", ka); // An acceleration of 1 rps/s requires 0.01 V output
+    slot0configs.kP = SmartDashboard.getNumber("kP", kp); // A position error of 2.5 rotations results in 12 V output
+    slot0configs.kI = SmartDashboard.getNumber("kI", ki); // no output for integrated error
+    slot0configs.kD = SmartDashboard.getNumber("kD", kd);
+    //motorConfig.MotionMagic.MotionMagicCruiseVelocity = SmartDashboard.getNumber("MMCruiseVelocity", 0);
+    //motorConfig.MotionMagic.MotionMagicAcceleration = SmartDashboard.getNumber("MMAcceleration", 160);
+    //motorConfig.MotionMagic.MotionMagicJerk = SmartDashboard.getNumber("MMJerk", 1600);
+    motor.getConfigurator().apply(slot0configs);
+    
+    motor.setControl(new VelocityVoltage(0).withSlot(0).withVelocity(SmartDashboard.getNumber("motor speed (rps)", 0)).withFeedForward(0));
+    //motor.set(SmartDashboard.getNumber("motor speed (percent)", 0.25));
   }
 
   @Override
